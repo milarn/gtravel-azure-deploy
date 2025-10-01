6af0b50d2bc5ac2ed509b35d044abc3273067239
Martin Kjerrgard Lund <martin.lund@cipher.no>
2025-10-01 14:52:49 +0200

    utenbubblechart2

--- dashboard.js
+++ dashboard.js
@@ -198,11 +198,9 @@ function setupCardExports() {
             detailsBtn.innerHTML = `
                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                          d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"/>
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                          d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"/>
+                          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
                 </svg>
-                Map
+                Charts
             `;
         } else {
             detailsBtn.innerHTML = `
